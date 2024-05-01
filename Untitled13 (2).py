9ed7f19b3d70524ff772e2e028cc8925524abf31
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-01 08:13:48 +0530

    Add files via upload

---
+++ Untitled13 (2).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+s1 = "abcde"
+
+for i in range(97,123):
+    if chr(i) not in s1:
+        print(chr(i),end='')
+        
+ 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
